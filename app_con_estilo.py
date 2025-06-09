0eb328d2d1d91b7947670ba6ec4608a5a0849250
betho141 <162034006+betho141@users.noreply.github.com>
2025-06-09 14:57:23 -0400

    Update app_con_estilo.py
    terceras modificaciones

--- app_con_estilo.py
+++ app_con_estilo.py
@@ -36,9 +36,9 @@ def agregar_logo_lateral(imagen_url):
             background-image: url("{imagen_url}");
             background-repeat: no-repeat;
             background-attachment: fixed;
-            background-size: 220px;
-            background-position: top left;
-            padding-left: 240px;  /* desplaza el contenido a la derecha */
+            background-size: 400px;
+            background-position: 20px 40px;
+            padding-left: 420px;  /* desplaza el contenido a la derecha */
         }}
         </style>
         """,
